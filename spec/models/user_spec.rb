4d96b0216f1e28efc30a53e37d9538c3d71566d0
minori <astominori@mail>
2020-09-20 20:49:03 +0900

    create basic test case

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,40 +1,23 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                     :bigint           not null, primary key
-#  avatar                 :string(255)
-#  bio                    :string(255)
-#  confirmation_sent_at   :datetime
-#  confirmation_token     :string(255)
-#  confirmed_at           :datetime
-#  current_sign_in_at     :datetime
-#  current_sign_in_ip     :string(255)
-#  email                  :string(255)      default(""), not null
-#  encrypted_password     :string(255)      default(""), not null
-#  failed_attempts        :integer          default(0), not null
-#  last_sign_in_at        :datetime
-#  last_sign_in_ip        :string(255)
-#  locked_at              :datetime
-#  remember_created_at    :datetime
-#  reset_password_sent_at :datetime
-#  reset_password_token   :string(255)
-#  sign_in_count          :integer          default(0), not null
-#  unconfirmed_email      :string(255)
-#  unlock_token           :string(255)
-#  username               :string(255)
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#
-# Indexes
-#
-#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
-#  index_users_on_email                 (email) UNIQUE
-#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
-#  index_users_on_unlock_token          (unlock_token) UNIQUE
-#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "ユーザ名、メール、パスワードがあれば有効な状態であること" do
+    user = User.new(
+      username: "text",
+      email: "example@mail.com",
+      password: "password"
+    )
+
+    expect(user).to be_valid
+  end
+
+  it "メールがない場合は無効な状態であること" do
+    user = User.new(
+      username: "text",
+      email: " ",
+      password: "password"
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("を入力してください")
+  end
 end
