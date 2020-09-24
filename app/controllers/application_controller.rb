b0c4cbc06884baef62a3bdb9514124afa4a798e2
minori <minori.astro317@gmail.com>
2020-09-24 21:15:56 +0900

    fix #2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  add_flash_types :success, :info, :warning, :danger
 
   protected
   # methodをオーバーライドする。
@@ -7,6 +8,6 @@ class ApplicationController < ActionController::Base
       # account_update, sign_in, sign_up, のフィールドを再定義
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username,:avatar,:bio])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username,:avatar,:bio])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username,:avatar,:bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar,:bio, :avatar_cache, :remove_avatar])
   end
 end
