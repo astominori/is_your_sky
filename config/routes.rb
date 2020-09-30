2a730edb0450cb66e07e60ed3765aa5079a75eb1
minori <minori.astro317@gmail.com>
2020-09-30 16:56:33 +0900

    fix #18

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "about", to: "home#about"
   get "/mypages", to: "mypages#show", as: :user_root
   get "todays_posts",to: "posts#todays_posts"
+  get "this_months_posts", to: "posts#this_months_posts"
   resources :posts
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
