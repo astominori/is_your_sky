6b1e1b707707b7f5dabf6773aa669178af6cb64f
minori <minori.astro317@gmail.com>
2020-10-03 15:51:53 +0900

    create seed file fix #22

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -12,13 +12,13 @@
   sample1 = User.create!(username: "testuser",
                          email: "example-1@email.com",
                          password: "password",
-                         avatar: File.open("#{Rails.root}/seed/sample-1.png"),
+                         avatar: File.open("#{Rails.root}/db/fixtures/sample-1.png"),
                          bio: "テストユーザです。")
 
   sample2 = User.create!(username: "山田一郎",
                          email: "example-2@email.com",
                          password: "password",
-                         avatar: File.open("#{Rails.root}/seed/sample-2.jpg"),
+                         avatar: File.open("#{Rails.root}/db/fixtures/sample-2.jpg"),
                          bio: "テストユーザです。山田です。")
 
   sample3 = User.create!(username: "ふくらP",
@@ -33,24 +33,24 @@
   Post.create!(user_id: sample1.id,
                title: "綺麗な空",
                text: "まるで映画の世界かのような美しさ",
-               image: File.open("#{Rails.root}/seed/image-1.jpg"),
+               image: File.open("#{Rails.root}/db/fixtures/image-1.jpg"),
                created_at: 1.week.ago.beginning_of_day)
   Post.create!(user_id: sample1.id,
                title: "天国へ向かう桟橋",
                text: "沈んでいく太陽と美しい茜色",
-               image: File.open("#{Rails.root}/seed/image-2.jpg"),
+               image: File.open("#{Rails.root}/db/fixtures/image-2.jpg"),
                created_at: 1.day.ago.beginning_of_day)
   Post.create!(user_id: sample1.id,
                title: "海と空の融合",
                text: "あまりにも綺麗な色合いで混ざり合う世界",
-               image: File.open("#{Rails.root}/seed/image-3.jpg"),
+               image: File.open("#{Rails.root}/db/fixtures/image-3.jpg"),
                created_at: Time.zone.now.beginning_of_day)
 
   10.times do |i|
     post = Post.create!(user_id: sample2.id,
                         title: "テストその#{i}",
                         text: "テストポストです。#{i}",
-                        image: File.open("#{Rails.root}/seed/image-#{i+4}.jpg"),
+                        image: File.open("#{Rails.root}/db/fixtures/image-#{i+4}.jpg"),
                         created_at: 1.day.ago.beginning_of_day)
     tag_list = ["空","綺麗な景色"]
     post.save_tags(tag_list)
@@ -59,20 +59,20 @@
   post1 = Post.create!(user_id: sample3.id,
                        title: "きれい〜",
                        text: "オレンジの建物がアクセントになりますね",
-                       image: File.open("#{Rails.root}/seed/image-15.jpg"),
+                       image: File.open("#{Rails.root}/db/fixtures/image-15.jpg"),
                        created_at: 1.week.ago.beginning_of_day)
   post1.save_tags(["青空","建物"])
 
   post2 = Post.create!(user_id: sample3.id,
                        title: "こんな空見たい",
                        text: "どんな星も見えてしまいそう。",
-                       image: File.open("#{Rails.root}/seed/image-16.jpg"),
+                       image: File.open("#{Rails.root}/db/fixtures/image-16.jpg"),
                        created_at: 1.day.ago.beginning_of_day)
   post2.save_tags(["星空"])
 
   post3 = Post.create!(user_id: sample3.id,
                        title: "夏だねぇ",
                        text: "どんな夏になるかなぁ",
-                       image: File.open("#{Rails.root}/seed/image-17.jpg"),
+                       image: File.open("#{Rails.root}/db/fixtures/image-17.jpg"),
                        created_at: Time.zone.now.beginning_of_day)
   post3.save_tags(["夏","入道雲"])
