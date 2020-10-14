cd69ec2db0321e2df86c8c62d5de39047232ca9d
minori <minori.astro317@gmail.com>
2020-10-14 17:29:52 +0900

    post controllerの変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,7 @@ class PostsController < ApplicationController
 
     #Vision APIの設定
     image_annotator = Google::Cloud::Vision.image_annotator do | config |
-      if Rails.env.production?
-        config.credentials = JSON.parse(ENV.fetch('GOOGLE_CREDENTIALS'))
-      else
-        config.credentials = ENV["GOOGLE_APPLICATION_CREDENTIALS"]
-      end
+       config.credentials = ENV["GOOGLE_APPLICATION_CREDENTIALS"]
     end
 
     #キャッシュ情報を取得する
