1280aba049a9ecda1530a106edb0f32599ab921f
minori <minori.astro317@gmail.com>
2020-10-23 20:37:52 +0900

    rspec修正 letの導入

--- spec/controllers/mypages_controller_spec.rb
+++ spec/controllers/mypages_controller_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe MypagesController, type: :controller do
   describe "#show" do
+    let(:user){ create(:user) }
+    let(:other_user){ create(:user, :other_user) }
+    let(:other_post){ create(:post, user_id: other_user.id) }
+
     before do
-      @user = create(:user)
-      @other_user = create(:user, :other_user)
-      @user.confirm
-      @other_user.confirm
-      @other_post = create(:post, user_id: @other_user.id)
+      user.confirm
+      other_user.confirm
     end
 
     context "投稿０の正しいユーザとして" do
       before do
-        sign_in @user
+        sign_in user
         get :show
       end
 
@@ -33,36 +34,36 @@ RSpec.describe MypagesController, type: :controller do
       end
 
       it "ユーザ以外が投稿した情報が取得できる" do
-        @other_post.updated_at.to_date
-        date = { date: @other_post }
+        other_post.updated_at.to_date
+        date = { date: other_post }
         expect(assigns[:posts_by_date]).to include { date }
       end
     end
 
     context "投稿済みの正しいユーザとして" do
+      let(:user_post){ create(:post, user_id: user.id) }
       before do
-        @user_post = create(:post, user_id: @user.id)
-        sign_in @user
+        sign_in user
         get :show
       end
 
       it "user_postsでユーザが投稿した情報が取得できる" do
-        expect(assigns(:user_posts)).to include @user_post
+        expect(assigns(:user_posts)).to include user_post
       end
 
       it "user_postsでユーザ以外が投稿した情報は取得できない" do
-        expect(assigns(:user_posts)).to_not include @other_post
+        expect(assigns(:user_posts)).to_not include other_post
       end
 
       it "posts_by_dateでユーザ以外が投稿した情報が取得できる" do
-        @other_post.updated_at.to_date
-        date = { date: @other_post }
+        other_post.updated_at.to_date
+        date = { date: other_post }
         expect(assigns[:posts_by_date]).to include { date }
       end
 
       it "posts_by_dateでユーザが投稿していない情報は取得できない" do
-        @other_post.updated_at.to_date
-        date = { date: @other_post }
+        other_post.updated_at.to_date
+        date = { date: other_post }
         expect(assigns[:posts_by_date]).to_not include { date }
       end
     end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,39 +2,40 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
   describe "#show" do
+    let(:user){ create(:user) }
+    let(:post){ create(:post, user_id: user.id) }
+
     before do
-      @user = create(:user)
-      @user.confirm
-      @post = create(:post, user_id: @user.id)
+      user.confirm
     end
 
     context "投稿したユーザとして" do
       before do
-        sign_in @user
+        sign_in user
       end
 
       it "正常にレスポンスを返すこと" do
-        get :show, params: { id: @post.id }
+        get :show, params: { id: post.id }
         expect(response).to be_successful
       end
     end
 
     context "投稿していない他のユーザとして" do
+      let(:other_user){ create(:user, :other_user) }
       before do
-        @other_user = create(:user, :other_user)
-        @other_user.confirm
-        sign_in @other_user
+        other_user.confirm
+        sign_in other_user
       end
 
       it "正常にレスポンスを返すこと" do
-        get :show, params: { id: @post.id }
+        get :show, params: { id: post.id }
         expect(response).to be_successful
       end
     end
 
     context "ゲストとして" do
       it "ログイン画面にリダイレクトする" do
-        get :show, params: { id: @post.id }
+        get :show, params: { id: post.id }
         expect(response).to redirect_to "/users/sign_in"
       end
     end
@@ -42,28 +43,29 @@ RSpec.describe PostsController, type: :controller do
 
   describe "#create" do
     context "正しいユーザとして" do
+      let(:user){ create(:user) }
+
       before do
-        @user = create(:user)
-        @user.confirm
+        user.confirm
       end
 
       context "有効な値を持つ場合" do
         it "ポストを追加できること" do
-          post_params = attributes_for(:post, user_id: @user)
-          sign_in @user
+          post_params = attributes_for(:post, user_id: user)
+          sign_in user
           expect {
             get :create, params: { post: post_params }
-          }.to change { @user.posts.count }.by(1)
+          }.to change { user.posts.count }.by(1)
         end
       end
 
       context "無効な値を持つ場合" do
         it "ポストを追加できないこと" do
-          post_params = attributes_for(:wrong_post, user_id: @user)
-          sign_in @user
+          post_params = attributes_for(:wrong_post, user_id: user)
+          sign_in user
           expect {
             get :create, params: { post: post_params }
-          }.to_not change(@user.posts, :count)
+          }.to_not change(user.posts, :count)
         end
       end
     end
@@ -78,87 +80,87 @@ RSpec.describe PostsController, type: :controller do
   end
 
   describe "#edit" do
+    let(:user){ create(:user) }
+    let(:post){ create(:post, user_id: user.id) }
     before do
-      @user = create(:user)
-      @user.confirm
-      @post = create(:post, user_id: @user.id)
+      user.confirm
     end
 
     context "投稿ユーザとして" do
       it "正常なレスポンスを返す" do
-        sign_in @user
-        get :edit, params: { id: @post.id }
+        sign_in user
+        get :edit, params: { id: post.id }
         expect(response).to be_successful
       end
 
       it "200レスポンスを返す" do
-        sign_in @user
-        get :edit, params: { id: @post.id }
+        sign_in user
+        get :edit, params: { id: post.id }
         expect(response).to have_http_status(200)
       end
 
       it "editテンプレートを描画する" do
-        sign_in @user
-        get :edit, params: { id: @post.id }
+        sign_in user
+        get :edit, params: { id: post.id }
         expect(response).to render_template(:edit)
       end
     end
 
     context "投稿していないユーザとして" do
+      let(:other_user){ create(:user, :other_user) }
       before do
-        @other_user = create(:user, :other_user)
-        @other_user.confirm
+        other_user.confirm
       end
 
       it "マイページ画面にリダイレクトする" do
-        sign_in @other_user
-        get :edit, params: { id: @post.id }
+        sign_in other_user
+        get :edit, params: { id: post.id }
         expect(response).to redirect_to user_root_path
       end
     end
 
     context "ゲストとして" do
       it "ログイン画面にリダイレクトする（devise既定）" do
-        get :edit, params: { id: @post.id }
+        get :edit, params: { id: post.id }
         expect(response).to redirect_to "/users/sign_in"
       end
     end
   end
 
   describe "#update" do
+    let(:post){ create(:post, user_id: user.id) }
+    let(:user){ create(:user) }
     before do
-      @user = create(:user)
-      @user.confirm
-      @post = create(:post, user_id: @user.id)
+      user.confirm
     end
 
     context "投稿ユーザとして" do
       it "投稿を更新できること" do
         post_params = attributes_for(
           :post,
-          user_id: @user.id,
+          user_id: user.id,
           title: "change the title"
         )
-        sign_in @user
-        get :update, params: { id: @post.id, post: post_params }
-        expect(@post.reload.title).to eq "change the title"
+        sign_in user
+        get :update, params: { id: post.id, post: post_params }
+        expect(post.reload.title).to eq "change the title"
       end
     end
 
     context "投稿していないのユーザとして" do
+      let(:other_user){ create(:user, :other_user) }
       before do
-        @other_user = create(:user, :other_user)
-        @other_user.confirm
+        other_user.confirm
       end
 
       it "マイページ画面にリダイレクトされること" do
         post_params = attributes_for(
           :post,
-          user_id: @user.id,
+          user_id: user.id,
           title: "change the title"
         )
-        sign_in @other_user
-        get :update, params: { id: @post.id, post: post_params }
+        sign_in other_user
+        get :update, params: { id: post.id, post: post_params }
         expect(response).to redirect_to user_root_path
       end
     end
@@ -167,10 +169,10 @@ RSpec.describe PostsController, type: :controller do
       it "ログイン画面にリダイレクトする（devise既定）" do
         post_params = attributes_for(
           :post,
-          user_id: @user.id,
+          user_id: user.id,
           title: "change the title"
         )
-        get :update, params: { id: @post.id, post: post_params }
+        get :update, params: { id: post.id, post: post_params }
         expect(response).to redirect_to "/users/sign_in"
       end
     end
@@ -178,32 +180,32 @@ RSpec.describe PostsController, type: :controller do
 
   describe "#todays_posts" do
     context "正しいユーザとして" do
+      let(:user){ create(:user) }
+      let(:posts){ create_list(:post, 5, user_id: user.id) }
       before do
-        @user = create(:user)
-        @posts = create_list(:post, 5, user_id: @user.id)
-        @user.confirm
+        user.confirm
       end
 
       it "正常にレスポンスを返すこと" do
-        sign_in @user
+        sign_in user
         get :todays_posts
         expect(response).to be_successful
       end
 
       it "200レスポンスを返すこと" do
-        sign_in @user
+        sign_in user
         get :todays_posts
         expect(response).to have_http_status "200"
       end
 
       it "todays_postsが投稿を5つ保持すること" do
-        sign_in @user
+        sign_in user
         get :todays_posts
-        expect(assigns[:todays_posts]).to eq @posts
+        expect(assigns[:todays_posts]).to eq posts
       end
 
       it "todays_postsのテンプレートを返すこと" do
-        sign_in @user
+        sign_in user
         get :todays_posts
         expect(response).to render_template("todays_posts")
       end
