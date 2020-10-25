b4d1611721cd5a3000ca9be5808a0d525d9f75da
minori <minori.astro317@gmail.com>
2020-10-25 21:40:56 +0900

    fix #49 mypages_controllerのtypo修正

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -3,7 +3,7 @@ class MypagesController < ApplicationController
 
   def show
     @user_posts = current_user.posts.all.order(created_at: "DESC")
-    all_posts = Post.where.not(id: current_user.id).order(created_at: "DESC")
+    all_posts = Post.where.not(user_id: current_user.id).order(created_at: "DESC")
     @posts_by_date = all_posts.group_by { |post| post.created_at.to_date }
   end
 end
